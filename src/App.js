ef29de9f34bda678abaca6ecdecbd4185e07c71a
SakilMostak <skmahim71@gmail.com>
2022-11-11 20:47:25 +0530

    created the body component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import {Head} from './components/Head'
 import {Body} from './components/Body'
-import {Footer} from './components/Footer'
+
 import './App.css'
 function App() {
   return (
     <div className="App">
       <Head/>
       <Body/>
-      <Footer/>
     </div>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,21 @@
+import './Body.css'
 
 export const Body = () => {
     return (
         <div>
-            <h1>body</h1>
+            <p className="psection">
+                Linear Data Structure is the most commonly used method found in Computer Science. 
+                Array as well as Linked-List are the bane of linear data structure. 
+                In this article, we will distinguish between the both and set out to see the benefit of both world.
+            </p>
+            <h2 className='artHead'>
+                Array:
+            </h2>
+            <p className="psection">
+            Array is a container which can hold a fix number of items of same data type.
+            It is created by assigning pointer to a contiguous block of memory. The memory is divided into fixed length buckets
+            depending on the data type used. The pointer is used to access the data present at each bucket.
+            </p>
         </div>
     )
 }
